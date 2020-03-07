7487b6d9f804a279f36013da1402cd3ed5f041fc
brangkak <brangkakk@gmail.com>
2020-03-07 22:02:05 +0800

    phpdasar

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table>
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>
+                <a href="">ubah</a> |
+                <a href="">hapus</a>
+            </th>
+            <th>paozan.jpg</th>
+            <th>18TI043</th>
+            <th>Paozan Wadi</th>
+            <th>fausan@gmail.com</th>
+            <th>Teknik Informatika</th>
+        </tr>
+
+    </table>
+</body>
+</html>
