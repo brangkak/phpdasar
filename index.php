ea9530ca3dd7a3ed780265b34f774e43bc1fbd0f
brangkak <brangkakk@gmail.com>
2020-03-07 23:04:03 +0800

    menghubungkan, memanggil data

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+    // menghubungkan file index ke function
+    require 'function/functions.php';
+
+    // megirim data menggunakan fungsi
+    $mahasiswa = query("SELECT * FROM mahasiswa");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,29 +16,34 @@
 </head>
 <body>
     <h1>Daftar Mahasiswa</h1>
-    <table>
+    <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>No.</th>
             <th>Aksi</th>
             <th>Gambar</th>
             <th>NRP</th>
             <th>Nama</th>
-            <th>Email</th>
             <th>Jurusan</th>
+            <th>Email</th>
         </tr>
-        <tr>
-            <th>1</th>
-            <th>
-                <a href="">ubah</a> |
-                <a href="">hapus</a>
-            </th>
-            <th>paozan.jpg</th>
-            <th>18TI043</th>
-            <th>Paozan Wadi</th>
-            <th>fausan@gmail.com</th>
-            <th>Teknik Informatika</th>
-        </tr>
-
+        <!-- mendeklarasikan angka -->
+        <?php $i = 1; ?>
+        <!-- menggunakan perulangan khusus array di php -->
+        <?php foreach( $mahasiswa as $mhs ) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="">ubah</a> |
+                    <a href="">hapus</a>
+                </td>
+                <td><?= $mhs["gambar"]; ?></td>
+                <td><?= $mhs["nrp"]; ?></td>
+                <td><?= $mhs["nama"]; ?></td>
+                <td><?= $mhs["jurusan"]; ?></td>
+                <td><?= $mhs["email"]; ?></td>
+            </tr>
+        <?php $i++; ?>
+        <?php endforeach; ?>
     </table>
 </body>
 </html>
